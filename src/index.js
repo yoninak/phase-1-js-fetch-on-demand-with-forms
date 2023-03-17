1c90a22e1a162ebb834c55cd93d8dac35f676ec4
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-03-17 16:08:12 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+      
+        fetch(`http://localhost:3000/movies/${input.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+          });
+      });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
 
-document.addEventListener('DOMContentLoaded', init);
+  
